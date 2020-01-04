e00fc1a9400d325ce8e33763c435d0d80784b6d7
Daniel Christensen <dchri18@gmail.com>
2020-01-04 14:59:57 +1000

    Very Basic Encryption Using AES for symmetric encryptic. This will be used to encrypt serialised JSON objects so they cannot be messed with once saved onto local disk.

--- Code/Framework/Utilities/Encryption.cs
+++ Code/Framework/Utilities/Encryption.cs
@@ -1,10 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
+using System.Security.Cryptography;
 
 namespace Framework.Utilities
 {
-    class Encryption
+    public static class Encryption
     {
+        private static byte[] _key;
+        private static byte[] _iv;
+
+        /// <summary>
+        /// Initialises the Encryption class with a Key and Initialisation Vector.
+        /// </summary>
+        /// <param name="key">The Key.</param>
+        /// <param name="iv">The Initialisation Vector.</param>
+        public static void Initialise(byte[] key, byte[] iv)
+        {
+            _key = key;
+            _iv = iv;
+        }
+
+        /// <summary>
+        /// Using AES - the string plaintext is encrypted to a byte stream.
+        /// </summary>
+        /// <param name="plaintext">The string to encrypt.</param>
+        /// <returns>The Encrypted string.</returns>
+        public static byte[] EncryptString(string plaintext)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = _key;
+                aes.IV = _iv;
+
+                ICryptoTransform encrypter = aes.CreateEncryptor(_key, _iv);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encrypter, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plaintext);
+                        }
+
+                        return msEncrypt.ToArray();
+                    }
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Using AES - the byte stream is decrypted and returned as a string.
+        /// </summary>
+        /// <param name="ciphertext">The byte stream to decrypt.</param>
+        /// <returns>The decrypted string.</returns>
+        public static string DecryptString(byte[] ciphertext)
+        {
+            string plaintext = null;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = _key;
+                aes.IV = _iv;
+
+                ICryptoTransform decrypter = aes.CreateDecryptor(_key, _iv);
+
+                using (MemoryStream msDecrypt = new MemoryStream(ciphertext))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decrypter, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return plaintext;
+        }
     }
 }
---
+++ Code/Testing/EncryptionTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Framework.Utilities.Encryption;
+
+namespace Testing
+{
+    [TestClass]
+    public class EncryptionTests
+    {
+        [TestMethod]
+        public void EncryptAndDecrypt()
+        {
+            byte[] key = Encoding.ASCII.GetBytes("CE0581F19725CA80C9C2F31BCF2E734A");
+            byte[] iv = Encoding.ASCII.GetBytes("454CC630A49029B2");
+
+            Initialise(key, iv);
+
+            string plaintext = "Thisisateststring";
+
+            byte[] ciphertext = EncryptString(plaintext);
+
+            Assert.AreEqual(plaintext, DecryptString(ciphertext));
+        }
+
+        [TestMethod]
+        public void FailEncryptAndDecrypt()
+        {
+            byte[] key = Encoding.ASCII.GetBytes("CE0581F19725CA80C9C2F31BCF2E734A");
+            byte[] iv = Encoding.ASCII.GetBytes("454CC630A49029B2");
+
+            Initialise(key, iv);
+
+            string plaintext = "Thisisateststring";
+
+            byte[] ciphertext = EncryptString(plaintext);
+
+            Assert.AreNotEqual("Thisisateststring1", DecryptString(ciphertext));
+        }
+    }
+}
